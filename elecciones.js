bf46489e122d3dc7e09a6a6b52fa9f4cae098770
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2023-03-15 17:57:52 +0100

    final

--- elecciones.js
+++ elecciones.js
@@ -4,33 +4,49 @@ const select_oeste = document.getElementById('oeste');
 
 // Función para llenar la tabla con los datos correspondientes
 function llenarTabla_este() {
-const select_este = document.getElementById('este');
-const select_oeste = document.getElementById('oeste');
-var valor = select_este.value;
-console.log(document.getElementById('base_lebron'));
-if(document.getElementById('base_lebron').textContent == ""){
-    document.getElementById('base_lebron').textContent = valor;
-}else if(document.getElementById('base_giannis').textContent == ""){
-    document.getElementById('base_giannis').textContent = valor;
-}else if(document.getElementById('escolta_lebron').textContent == ""){
-    document.getElementById('escolta_lebron').textContent = valor;
-}else if(document.getElementById('escolta_giannis').textContent == ""){
-    document.getElementById('escolta_giannis').textContent = valor;
-}else if(document.getElementById('alero_lebron').textContent == ""){
-    document.getElementById('alero_lebron').textContent = valor;
-}else if(document.getElementById('alero_giannis').textContent == ""){
-    document.getElementById('alero_giannis').textContent = valor;
-}else if(document.getElementById('ala_lebron').textContent == ""){
-    document.getElementById('ala_lebron').textContent = valor;
-}else if(document.getElementById('ala_giannis').textContent == ""){
-    document.getElementById('ala_giannis').textContent = valor;
-}else if(document.getElementById('pivot_lebron').textContent == ""){
-    document.getElementById('pivot_lebron').textContent = valor;
-}else if(document.getElementById('pivot_giannis').textContent == ""){
-    document.getElementById('pivot_giannis').textContent = valor;
-}else{
-    alerta();
-}
+    const select_este = document.getElementById('este');
+    const select_oeste = document.getElementById('oeste');
+    var valor = select_este.value;
+    console.log(document.getElementById('base_lebron'));
+    if (document.getElementById('base_lebron').textContent == "") {
+        document.getElementById('base_lebron').textContent = valor;
+    } else if (document.getElementById('base_giannis').textContent == "") {
+        document.getElementById('base_giannis').textContent = valor;
+    } else if (document.getElementById('escolta_lebron').textContent == "") {
+        document.getElementById('escolta_lebron').textContent = valor;
+    } else if (document.getElementById('escolta_giannis').textContent == "") {
+        document.getElementById('escolta_giannis').textContent = valor;
+    } else if (document.getElementById('alero_lebron').textContent == "") {
+        document.getElementById('alero_lebron').textContent = valor;
+    } else if (document.getElementById('alero_giannis').textContent == "") {
+        document.getElementById('alero_giannis').textContent = valor;
+    } else if (document.getElementById('ala_lebron').textContent == "") {
+        document.getElementById('ala_lebron').textContent = valor;
+    } else if (document.getElementById('ala_giannis').textContent == "") {
+        document.getElementById('ala_giannis').textContent = valor;
+    } else if (document.getElementById('pivot_lebron').textContent == "") {
+        document.getElementById('pivot_lebron').textContent = valor;
+    } else if (document.getElementById('pivot_giannis').textContent == "") {
+        document.getElementById('pivot_giannis').textContent = valor;
+    } else {
+        alerta();
+    }
+
+    const alertPlaceholder = document.getElementById('AlertPlaceholder')
+
+    const alert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
+
+    alert('Jugador añadido correctamente!', 'success');
 
 }
 
@@ -39,36 +55,50 @@ function llenarTabla_oeste() {
     const select_oeste = document.getElementById('oeste');
     var valor = select_oeste.value;
     console.log(document.getElementById('base_lebron'));
-    if(document.getElementById('base_lebron').textContent == ""){
+    if (document.getElementById('base_lebron').textContent == "") {
         document.getElementById('base_lebron').textContent = valor;
-    }else if(document.getElementById('base_giannis').textContent == ""){
+    } else if (document.getElementById('base_giannis').textContent == "") {
         document.getElementById('base_giannis').textContent = valor;
-    }else if(document.getElementById('escolta_lebron').textContent == ""){
+    } else if (document.getElementById('escolta_lebron').textContent == "") {
         document.getElementById('escolta_lebron').textContent = valor;
-    }else if(document.getElementById('escolta_giannis').textContent == ""){
+    } else if (document.getElementById('escolta_giannis').textContent == "") {
         document.getElementById('escolta_giannis').textContent = valor;
-    }else if(document.getElementById('alero_lebron').textContent == ""){
+    } else if (document.getElementById('alero_lebron').textContent == "") {
         document.getElementById('alero_lebron').textContent = valor;
-    }else if(document.getElementById('alero_giannis').textContent == ""){
+    } else if (document.getElementById('alero_giannis').textContent == "") {
         document.getElementById('alero_giannis').textContent = valor;
-    }else if(document.getElementById('ala_lebron').textContent == ""){
+    } else if (document.getElementById('ala_lebron').textContent == "") {
         document.getElementById('ala_lebron').textContent = valor;
-    }else if(document.getElementById('ala_giannis').textContent == ""){
+    } else if (document.getElementById('ala_giannis').textContent == "") {
         document.getElementById('ala_giannis').textContent = valor;
-    }else if(document.getElementById('pivot_lebron').textContent == ""){
+    } else if (document.getElementById('pivot_lebron').textContent == "") {
         document.getElementById('pivot_lebron').textContent = valor;
-    }else if(document.getElementById('pivot_giannis').textContent == ""){
+    } else if (document.getElementById('pivot_giannis').textContent == "") {
         document.getElementById('pivot_giannis').textContent = valor;
-    }else{
+    } else {
         alerta();
     }
-    
+
+    const alertPlaceholder = document.getElementById('AlertPlaceholder')
+
+    const alert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
     }
+    alert('Jugador añadido correctamente!', 'success');
+
+}
 
 
 
-function alerta()
-    {
+function alerta() {
     var mensaje;
     var opcion = confirm("Desea reiniciar?");
     if (opcion == true) {
@@ -85,11 +115,13 @@ function alerta()
 
         alert("!Comienza de nuevo¡")
 
-	} else {
-	    alert("!Gracias¡")
-	}
-	
+    } else {
+        alert("!Gracias¡")
+    }
+
 }
+
+
 
 
 
